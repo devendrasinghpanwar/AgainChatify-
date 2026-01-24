325ca0c26930906b720857da37351b3b272f552a
dev <107792454+devendrasinghpanwar@users.noreply.github.com>
2026-01-24 19:35:21 +0530

    now ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,4 @@
-// const express = require('express');
+/* // const express = require('express');
 import express from "express"; // 
 import dotenv from 'dotenv';
 import path from 'path';
@@ -28,4 +28,37 @@ if(process.env.NODE_ENV==='production'){
 }
 
 app.listen(PORT ,()=>console.log("Server is running on port :"+PORT));  
+*/
 
+
+
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+const __dirname = path.resolve();
+const PORT = process.env.PORT || 3000;
+
+// API routes
+app.use("/api/auth", authRoutes);
+app.use("/api/auth", messageRoute);
+
+// Production setup
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    express.static(path.join(__dirname, "frontend/dist"))
+  );
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "frontend", "dist", "index.html")
+    );
+  });
+}
+
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
