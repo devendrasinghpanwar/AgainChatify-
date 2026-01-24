318a6ab9470b118ef60342cdd5f3421a902cb158
dev <107792454+devendrasinghpanwar@users.noreply.github.com>
2026-01-24 18:54:59 +0530

    ready for deployement

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,31 @@
 // const express = require('express');
 import express from "express"; // 
 import dotenv from 'dotenv';
+import path from 'path';
 import authRoutes from './routes/authRoutes.js';
 import messageRoute from './routes/messageRoute.js'
+
 const app= express();
 
-dotenv.config(); // configuration for .env file 
- const PORT = process.env.PORT || 3000;
 
+const __dirname=path.resolve();
+
+dotenv.config(); // configuration for .env file 
+// console.log(dotenv.config());
+ const PORT = process.env.PORT || 3000;
 
 app.use('/api/auth',authRoutes);
 app. use('/api/auth',messageRoute);
 
+// make  ready for deployement 
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname,"../frontend/dist"))); // basically this path is used for fetching the dist file(deployable) from frontend
+
+    app.get("*",(_,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html")); // this path is resolve the problem 
+    })
+
+}
+
 app.listen(PORT ,()=>console.log("Server is running on port :"+PORT));  
 
- 
