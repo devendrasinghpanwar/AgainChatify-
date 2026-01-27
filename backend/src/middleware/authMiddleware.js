f88686bcd473f093f721603b444c55bcb72ba16b
dev <107792454+devendrasinghpanwar@users.noreply.github.com>
2026-01-27 22:02:10 +0530

    created protect & updateProfile

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,32 @@
+// my algorithms to check the user is avaliable in databse or not 
+import User from "../models/User.js";
+import { ENV } from "../lib/env.js";
+import jwt from "jsonwebtoken";
+
+
+export const protectRoute =async(req,res,next)=>{ // it will take three arguments req( which is used for fetching data) , res for giving response to frontend from backend , and next is used for next () function which is used for calling next method ( function ) which on queue 
+    // user  is exist of not in our databse
+    try {
+ 
+        const token = req.cookies.jwt; // fetch token from  database
+        if(!token) return res.status(401).json({message:"Unauthorized: no token provided"});
+
+        // after getting token decode(varify) it 
+        
+        const decoded = jwt.verify(token,ENV.JWT_SECRET); // varify whatever token you got from  databse using jwt   
+        if(!decoded) return res.status(401).json({message:"Unauthorized Invalid Token"});
+
+        // we've varified user token now it's time fo user 
+
+        const user = await User.findById(decoded.userId).select("-password"); // find UserId from User model ( from databse) and leave password there 
+        if(!user) return res.status(401).json({message:"User not found"}); 
+
+        req.user=user; // if we found everything so put all this user in req.user ( which means current user)
+        next(); //  call the next function is "function updateProfile" in authRoutes  
+
+    } catch (error) {
+        console.error("Error is protectRoute middleware: ",error);
+        res.status(500).json({message:"Internal server error"});
+        
+    }
+}
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { signup,login,logout } from "../controller/authController.js";
-
+import { signup,login,logout,updateProfile} from "../controller/authController.js";
+import { protectRoute } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.post("/signup", signup);
@@ -9,4 +9,7 @@ router.post("/login",login);
 
 router.post("/logout",logout);
 
+
+router.put('/update-profile',protectRoute,updateProfile);
+router.get('/check',protectRoute,(req,res)=>res.status(200).json(req.user));// we will request the user after checking 
 export default router;
