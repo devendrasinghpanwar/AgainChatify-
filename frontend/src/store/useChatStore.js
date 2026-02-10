5bd0f3e78a26bed48c9a5715f6c95e54722911cc
dev <107792454+devendrasinghpanwar@users.noreply.github.com>
2026-02-10 18:51:09 +0530

    Chat-Container-Setup-Done

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -44,17 +44,21 @@ export const useChatStore = create((set, get) => ({
     }
   },
 
-  // getMessagesByUserId: async (userId) => {
-  //   set({ isMessagesLoading: true });
-  //   try {
-  //     const res = await axiosInstance.get(`/messages/${userId}`);
-  //     set({ messages: res.data });
-  //   } catch (error) {
-  //     toast.error(error.response?.data?.message || "Something went wrong");
-  //   } finally {
-  //     set({ isMessagesLoading: false });
-  //   }
-  // },
+
+  getMessagesByUserId: async (userId)=>{
+    set({isMessagesLoading:true});
+    try {
+      const res = await axiosInstance.get(`/messages/${userId}`);
+      set({messages:res.data}) ;
+    } catch (error) {
+     toast.error(error?.response?.message||"something went wrong") 
+    }finally{
+
+      set({isMessagesLoading:false});
+    }
+  } ,
+
+  
 
   // sendMessage: async (messageData) => {
   //   const { selectedUser, messages } = get();
